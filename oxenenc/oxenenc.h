091a5ea16c1a4a6ce42a5015322185b7a8a11c09
Jeff Becker <jeff@i2p.rocks>
2021-10-15 10:19:30 -0400

    add all the source from oxenmq

---
+++ oxenenc/oxenenc.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "bt_serialize.h"
+#include "bt_value.h"
+#include "base32z.h"
+#include "base64.h"
+#include "hex.h"
---
+++ tests/common.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <catch2/catch.hpp>
+#include "oxenenc/oxenenc.h"
+
+using namespace oxenenc;
