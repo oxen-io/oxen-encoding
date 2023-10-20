443b1ce231a2b36eaedb9a4aae7edf2f4a236992
Jason Rhinelander <jason@imaginary.ca>
2023-10-20 16:44:04 -0300

    Remove unused/unreliable current_buffer()
    This doesn't appear to have ever been used.  It's also not something we
    really want to expose; the internal string_view is meant to be a private
    implementation detail of the consumer.

--- oxenc/bt_serialize.h
+++ oxenc/bt_serialize.h
@@ -761,9 +761,6 @@ class bt_list_consumer {
     bt_list_consumer(const bt_list_consumer&) = default;
     bt_list_consumer& operator=(const bt_list_consumer&) = default;
 
-    /// Get a copy of the current buffer
-    std::string_view current_buffer() const { return data; }
-
     /// Returns true if the next value indicates the end of the list
     bool is_finished() const { return data.front() == 'e'; }
     /// Returns true if the next element looks like an encoded string
